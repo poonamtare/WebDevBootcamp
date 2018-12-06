ac9fb7c52203ee6cf877c8836a4b6654f00d821e
ptare <pnmtare@gmail.com>
2018-12-06 07:53:34 +0000

    add lesson on route parameters

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -17,6 +17,21 @@ app.get("/dogs", function(req, res){
 });
 
 
+//In order to create patterns for route parameters we use ':'
+app.get("/r/:subredditName", function(req, res){
+    var subreddit = req.params.subredditName; 
+   res.send(" Welcome to the " + subreddit.toUpperCase() + " page" ); 
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res){
+   res.send("Welcome to the comment section!"); 
+});
+
+app.get("*", function(req, res){
+    res.send("You are a star!!...");
+});
+
 // Tell express to listen to requests (start server)
 
-app.listen(process.env.PORT,process.env.IP, function(){console.log("Server has started!...")});
+app.listen(process.env.PORT,process.env.IP, function(){
+    console.log("Server has started!...")});
